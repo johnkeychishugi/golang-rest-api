6563149007b048aa226c77125e205e986a8a5016
John Chishugi <jkchishugi@gmail.com>
2022-08-22 17:12:06 +0200

    ft-dataabase-connection

---
+++ config/database-config.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func setUpDatabaseConnection() *gorm.DB {
+	errEnv := godotenv.Load()
+
+	if errEnv != nil {
+		panic("Failed to load env file")
+	}
+
+	dbUser := os.Getenv("DB_USERNAME")
+	dbPass := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbName := os.Getenv("DB_DATABASE")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?charset=utf8&parseTime=True&loc=Local", dbUser, dbPass, dbHost, dbName)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("Failed to create a connection to the DB")
+	}
+
+	return db
+}
+
+func closeDatabaseConnection(db *gorm.DB) {
+	dbSQL, err := db.DB()
+
+	if err != nil {
+		panic("Failed to close connection from the database")
+	}
+
+	dbSQL.Close()
+}
